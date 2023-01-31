086ee771c8827b77f226451d07ebad03836e907b
Ligia <ligiakamyllephb@gmail.com>
2023-01-31 20:17:04 -0300

    Adiciona Classe Produto

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+class  Produto:
+    def __init__(self, nome, preço) -> None:
+        self.nome = nome
+        self.preço = preço
+        
+
 quantidade = int(input("Informe a quantidade: "))
 print(quantidade)
