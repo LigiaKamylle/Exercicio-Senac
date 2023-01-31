ae065a2520882d2f9d924978ef25be9b56fbf807
Ligia <ligiakamyllephb@gmail.com>
2023-01-31 20:41:35 -0300

    Adiciona novo atributo
    Foi adicionado um atributo novo para que seja possivel saber o país de origem

--- produtos/main.py
+++ produtos/main.py
@@ -1,7 +1,8 @@
 class  Produto:
-    def __init__(self, nome, preço) -> None:
+    def __init__(self, nome, preço, origem) -> None:
         self.nome = nome
         self.preço = preço
+        self.origem = origem
         
 
 quantidade = int(input("Informe a quantidade: "))
